30d57d516e5d228e610bca4763de72d2254206aa
wonseok-do <wsdo970@gmail.com>
2022-08-05 20:44:22 +0900

    add리듀서추가

---
+++ src/redux/modules/todos.js
@@ -0,0 +1,26 @@
+const ADDTODO = "todo/ADDTODO";
+
+export function addTodo(todo) {
+  return { type: ADDTODO, todo };
+}
+
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      userName: "아무개",
+      title: "밥 먹자",
+      comment: "어디에서 먹을까요?",
+    },
+  ],
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "todo/ADDTODO": {
+      return { ...state, todos: [...state.todos, action.todo] };
+    }
+    default:
+      return state;
+  }
+}
