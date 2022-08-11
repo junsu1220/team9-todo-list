4a798956b00a1a6d60b272ea7df06148003d5763
crystal025 <oj3339@naver.com>
2022-08-11 21:55:46 +0900

    Detail-update

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -9,22 +9,22 @@ const Detail = () => {
   const [modal, setModal] = useState(false);
   const todoList = useSelector((state) => state.todos.todos);
   const navigate = useNavigate();
+  const {id} = useParams();
+  const posts = todoList.find((post)=>post.id == id)
 
   return (
     <div className="wrap-box">
-      {todoList.map((todoitem) => {
-        return (
           <>
-            <div className="id-box" key={`${todoitem.id}`}>
-              <h3>id: {todoitem.id}</h3>
+            <div className="id-box" key={`${posts.id}`}>
+              <h3>id: {posts.id}</h3>
               <button className="home-btn"
               onClick={()=>{navigate(-1)}}>이전으로</button>
             </div>
             <div>
-              <h1>{todoitem.title}</h1>
+              <h1>{posts.title}</h1>
             </div>
             <div className="body-box">
-              <p>{todoitem.comment}</p>
+              <p>{posts.comment}</p>
             </div>
             {modal === false ? (
               <button className="comment-btn" onClick={() => setModal(true)}>
@@ -34,8 +34,6 @@ const Detail = () => {
 
             {modal === true ? <Comment setModal={setModal} /> : null}
           </>
-        );
-      })}
     </div>
   );
 };
